acb78b67a4d4b4c5292ea3cfbe60124f6004f873
uncleheyho <uncleheyho@cb6287ee-9fda-47c7-8af8-64e39e0d1b1d>
2013-02-06 10:15:37 +0000

    Добавлен каталог скриптов для автоматической сборки/публикации
    git-svn-id: https://svn.code.sf.net/p/v8reader/code/trunk@48 cb6287ee-9fda-47c7-8af8-64e39e0d1b1d

---
+++ build/getfiles.cmd
@@ -0,0 +1,7 @@
+md release
+erase /Q release\*
+
+copy ..\Release\v8viewer.exe release\v8viewer.exe
+copy ..\Release\FastColoredTextBox.dll release\FastColoredTextBox.dll
+copy ..\Release\CFReader.dll release\CFReader.dll
+copy ..\Release\v8viewer.exe.config release\v8viewer.exe.config
