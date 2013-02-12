3bcca9a31ee14484ac62e04ec7f2e1075b0d0d36
uncleheyho <uncleheyho@cb6287ee-9fda-47c7-8af8-64e39e0d1b1d>
2013-02-12 09:13:53 +0000

    git-svn-id: https://svn.code.sf.net/p/v8reader/code/trunk@56 cb6287ee-9fda-47c7-8af8-64e39e0d1b1d

--- build/getfiles.cmd
+++ build/getfiles.cmd
@@ -2,6 +2,6 @@ md release
 erase /Q release\*
 
 copy ..\Release\v8viewer.exe release\v8viewer.exe
-copy ..\Release\FastColoredTextBox.dll release\FastColoredTextBox.dll
+copy ..\Release\ICSharpCode.AvalonEdit.dll release\ICSharpCode.AvalonEdit.dll
 copy ..\Release\CFReader.dll release\CFReader.dll
 copy ..\Release\v8viewer.exe.config release\v8viewer.exe.config
