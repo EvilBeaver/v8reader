1b75afff6630c8521f0f1d61dc284ad80f9dea5a
uncleheyho <uncleheyho@cb6287ee-9fda-47c7-8af8-64e39e0d1b1d>
2013-02-20 09:34:17 +0000

    Скрипты сборки с автоинкрементом версии
    git-svn-id: https://svn.code.sf.net/p/v8reader/code/trunk@60 cb6287ee-9fda-47c7-8af8-64e39e0d1b1d

---
+++ build/build.cmd
@@ -0,0 +1,10 @@
+@echo off
+echo Build started
+
+call tools/setlocals.cmd
+
+%setter% "%VersionFile%"  -f_._.+._ -a_._.+._
+
+call "%vsvars%vsvars32.bat"
+devenv "%SolutionPath%" /build Release
+pause
---
+++ build/prebuild.cmd
@@ -0,0 +1,16 @@
+@echo off
+
+set setter=%~dp0\tools\vsetter.exe
+echo %setter%
+
+if %2 == Release goto release
+if %2 == Debug goto debug
+echo no action performed
+exit /B 5
+
+:debug
+%setter% "%1"  -f_._._.* -a_._._.*
+
+:release
+rem для релизной сборки инкремент будем запускать из скрипта
+rem %setter% "%1"  -f_._.+._ -a_._.+._
