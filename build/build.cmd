1f8f07272878f6dfede2d7d3a1c8f43448492d40
uncleheyho <uncleheyho@cb6287ee-9fda-47c7-8af8-64e39e0d1b1d>
2013-02-22 09:53:58 +0000

    При сборке релизной версии инкремент номера производится после сборки, а не до.
    git-svn-id: https://svn.code.sf.net/p/v8reader/code/trunk@66 cb6287ee-9fda-47c7-8af8-64e39e0d1b1d

--- build/build.cmd
+++ build/build.cmd
@@ -3,9 +3,10 @@ echo Build started
 
 call tools/setlocals.cmd
 
-%setter% "%VersionFile%"  -f_._.+._ -a_._.+._
-
 call "%vsvars%vsvars32.bat"
 devenv "%SolutionPath%" /build Release
 call getfiles.cmd
+
+%setter% "%VersionFile%"  -f_._.+.0 -a_._.+.0
+
 echo Done
