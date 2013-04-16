6152feb5179b0b1e2819bb0b2485ceb2ad1c7e69
uncleheyho <uncleheyho@cb6287ee-9fda-47c7-8af8-64e39e0d1b1d>
2013-04-16 12:11:36 +0000

    git-svn-id: https://svn.code.sf.net/p/v8reader/code/trunk@140 cb6287ee-9fda-47c7-8af8-64e39e0d1b1d

--- v8viewer/Properties/AssemblyInfo.cs
+++ v8viewer/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("V8 Viewer")]
+[assembly: AssemblyTitle("1C:Enterprise module viewer")]
 [assembly: AssemblyDescription("1C:Enterprise module viewer")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
